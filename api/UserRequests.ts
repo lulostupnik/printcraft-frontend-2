cf2d168ba6f30982946c0d00ca29fd4a04336c25
RamiroGarciaAr <67532049+RamiroGarciaAr@users.noreply.github.com>
2024-09-08 20:38:11 -0300

    User Requests Added

---
+++ api/UserRequests.ts
@@ -0,0 +1,42 @@
+import { API_URL } from "./api";
+
+
+export async function RegisterUser(username:string,password:string,email:string) 
+{
+    const ans = await fetch(`${API_URL}/register`,{
+            method: "POST",
+            body: JSON.stringify({
+                username,password,email
+            })
+        })
+}
+
+export async function LogInUser(username:string,password:string) 
+{
+    const ans = await fetch(`${API_URL}/login`,{
+            method: "POST",
+            body: JSON.stringify({
+                username,password
+            })
+        })
+}
+
+export async function CreateSeller(storeName:string,description:string,address:string) 
+{
+    const ans = await fetch(`${API_URL}/login`,{
+        method: "POST",
+        body: JSON.stringify({
+            storeName,description,address
+        })
+    })
+}
+
+export async function PublishProduct(name:string, material: string,stock:number,description:String,image_url:string,price:number) 
+{
+    const ans = await fetch(`${API_URL}/login`,{
+        method: "POST",
+        body: JSON.stringify({
+            name,material,stock,description,image_url,price
+        })
+    })
+}
