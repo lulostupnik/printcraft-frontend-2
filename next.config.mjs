5cc0f771ad4d88cc55ccc442acc2877c1419864d
lulardou <lulostupnik2003@gmail.com>
2024-10-06 11:49:06 -0300

    merge with mati

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,25 @@
 // export default nextConfig;
 
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//       domains: ['vvvlpyyvmavjdmfrkqvw.supabase.co'],
+//     },
+//   };
+  
+//   export default nextConfig;
+  
+// /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['vvvlpyyvmavjdmfrkqvw.supabase.co'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vvvlpyyvmavjdmfrkqvw.supabase.co',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
