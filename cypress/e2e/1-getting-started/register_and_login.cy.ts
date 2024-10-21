0173148f319efd460d40579d8e05b2de9566f201
lulardou <lulostupnik2003@gmail.com>
2024-10-20 23:17:08 -0300

    register

--- cypress/e2e/1-getting-started/register_and_login.cy.ts
+++ cypress/e2e/1-getting-started/register_and_login.cy.ts
@@ -1,19 +1,25 @@
 describe('Authentication', () => {
 
-    it('deberia poder registrarse', () => {
+  const name = 'Juan';
+  const surname = 'Perez';
+  const username = 'juancho222001';
+  const mail = 'juanrulo2@gmail.com';
+  const password = 'Juanrulo123'
 
+    it('deberia poder registrarse', () => {
+    
         cy.visit('/register');
-        cy.get('input[name="name"]').type('Juan');
+        cy.get('input[name="name"]').type(name);
         cy.wait(1000);
-        cy.get('input[name="surname"]').type('Perez');
+        cy.get('input[name="surname"]').type(surname);
         cy.wait(1000);
-        cy.get('input[name="username"]').type('juanrulo2');
+        cy.get('input[name="username"]').type(username);
         cy.wait(1000);
-        cy.get('input[name="email"]').type('juanrulo2@gmail.com');
+        cy.get('input[name="email"]').type(mail);
         cy.wait(1000);
-        cy.get('input[name="password"]').type('Juanrulo123');
+        cy.get('input[name="password"]').type(password);
         cy.wait(1000);
-        cy.get('input[name="confirmPassword"]').type('Juanrulo123');
+        cy.get('input[name="confirmPassword"]').type(password);
         cy.wait(1000);
         cy.get('button[type="submit"]').click();
         cy.wait(2000);
@@ -24,13 +30,13 @@ describe('Authentication', () => {
 
       cy.visit('/login');
       cy.wait(1000);
-      cy.get('input[name="username"]').type('juanrulo2');
+      cy.get('input[name="username"]').type(username);
       cy.wait(2000);
-      cy.get('input[name="password"]').type('Juanrulo123');
+      cy.get('input[name="password"]').type(password);
       cy.wait(1000);
       cy.get('button[type="submit"]').click();
       cy.wait(2000);
-      cy.contains('Bienvenido Juan');
+      cy.contains(`Bienvenido ${name}`);
       cy.wait(1000);
 
     });
