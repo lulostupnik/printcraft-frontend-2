cdaa389fee751ad3e6dadfbf7dcf9b2914179f73
lulardou <lulostupnik2003@gmail.com>
2024-11-28 13:43:19 -0300

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -49,34 +49,53 @@ export default function Footer({ className = '' }: FooterProps) {
 
   return (
     <footer className="bg-gray-800 text-gray-300 py-8">
-    <div className="max-w-7xl mx-auto">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-        <div>
+    <div className="max-w-7xl mx-auto px-4">
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+        {/* Comprar Section */}
+        <div className="lg:col-span-1">
           <h3 className="text-lg font-semibold mb-4">Comprar</h3>
           <ul>
-            <li className="mb-2"><a href="/products_catalog" className="hover:underline">Catálogo</a></li>
-            <li className="mb-2"><a href="/lookfor_designer" className="hover:underline">Diseñadores</a></li>
+            <li className="mb-2">
+              <a href="/products_catalog" className="hover:underline">
+                Catálogo
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="/lookfor_designer" className="hover:underline">
+                Diseñadores
+              </a>
+            </li>
           </ul>
         </div>
+  
+        {/* Vender Section */}
         <div>
           <h3 className="text-lg font-semibold mb-4">Vender</h3>
           <ul>
-            <li className="mb-2"><a onClick={handlePublishProductClick} style={{ cursor: 'pointer' }} className="hover:underline">Publicar</a></li>
-            <li className="mb-2"><a onClick={handleOfferServicesClick} style={{ cursor: 'pointer' }} className="hover:underline">Ofrecer Servicios</a></li>
+            <li className="mb-2">
+              <a onClick={handlePublishProductClick} style={{ cursor: "pointer" }} className="hover:underline">
+                Publicar
+              </a>
+            </li>
+            <li className="mb-2">
+              <a onClick={handleOfferServicesClick} style={{ cursor: "pointer" }} className="hover:underline">
+                Ofrecer Servicios
+              </a>
+            </li>
           </ul>
         </div>
-        <div>
-          <ul>
-            <li className="mb-2">FAQ</li>
-            <li className="mb-2">Terminos y Condiciones</li>
-            <li className="mb-2">Politica de Privacidad</li>
-          </ul>
-        </div>
-        <div>
-          <h3 className="text-lg font-semibold mb-4 text-right">&copy; 2023 Printcraft.<br/> All rights reserved.</h3>
+  
+        {/* Empty Space for Alignment */}
+        <div></div>
+  
+        {/* Copyright Section */}
+        <div className="text-right">
+          <h3 className="text-lg font-semibold mb-4">&copy; 2023 Printcraft.<br /> All rights reserved.</h3>
         </div>
       </div>
     </div>
   </footer>
+  
+  
   );
 }
